5516835ac028dbeddb97e41954efbfae108c6007
Aparyan <aryan.prakash.ug20@nsut.ac.in>
2022-08-10 18:06:20 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+        makeAList {" "} <span className="wave" role="img" aria-labelledby="wave"> 🗒 </span> 
+      <hr className="divider" />
+    </div>
+  );
+};
+
+export default Header;
